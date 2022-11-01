bd14b647ebc390c7473721d87f3ffb017c2db5c2
최선혁 <wafy75@gmail.com>
2022-11-01 20:22:31 +0900

    순수 JPA 기반 레포지토리 만들기

--- src/main/java/study/datajpa/repository/TeamRepository.java
+++ src/main/java/study/datajpa/repository/TeamRepository.java
@@ -2,6 +2,7 @@ package study.datajpa.repository;
 
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -9,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class MemberJpaRepository {
+public class TeamRepository {
 
     @PersistenceContext
     private EntityManager em;
@@ -23,23 +24,18 @@ public class MemberJpaRepository {
         em.remove(member);
     }
 
-    public Member find(Long id) {
-        return em.find(Member.class, id);
-    }
-
     public Optional<Member> findById(Long id) {
         Member member = em.find(Member.class, id);
         return Optional.ofNullable(member);
     }
 
     public long count() {
-        return em.createQuery("select count(m) from Member m", Long.class)
+        return em.createQuery("select count(t) from Team t", Long.class)
                 .getSingleResult();
     }
 
-    public List<Member> findAll() {
-        return em.createQuery("select m from Member m", Member.class)
+    public List<Team> findAll() {
+        return em.createQuery("select t from Team t", Team.class)
                 .getResultList();
     }
-
 }
