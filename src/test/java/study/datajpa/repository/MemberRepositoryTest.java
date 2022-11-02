9f60b179266711b8736c9487d402140fa15cfce3
최선혁 <wafy75@gmail.com>
2022-11-02 14:43:34 +0900

    레파지토리에 메소드쿼리 정의하기

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -84,4 +84,17 @@ class MemberRepositoryTest {
         assertThat(findMember).isEqualTo(member1);
     }
 
+    @Test
+    void testQuery() {
+
+        Member member1 = new Member("member1", 10);
+        Member member2 = new Member("member2", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findUser("member1", 10);
+        Member findMember = result.get(0);
+        assertThat(findMember).isEqualTo(member1);
+    }
+
 }
